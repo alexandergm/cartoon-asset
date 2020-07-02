c37176fe9197f0fea1f7c179eb463870f5069ffa
Shagrid <shagrid@yandex.ru>
2020-07-02 19:43:25 +0300

    Движение машинки, анимация звёзд

---
+++ Assets/CarMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMove : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5;
+
+    [SerializeField] private StarsController _starsController;
+    private Vector3 _movingVector;
+    private bool _isCarStopped = false;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(transform.position.z);
+        if (transform.position.z < 37.68f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + Time.deltaTime*_speed);
+            return;
+        }
+
+        if (!_isCarStopped)
+        {
+            _starsController.StartLevel();
+            _isCarStopped = true;
+        }
+        
+    }
+}
---
+++ Assets/CinematicController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinematicController : MonoBehaviour
+{
+    private StarsController _StarsController;
+
+    private void Start()
+    {
+        _StarsController = GetComponent<StarsController>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
